95c14d7155c8ef01e40ca4ac3ca8bf2a53d5353d
Mescxll <202411190002@ifba.edu.br>
2025-03-11 13:52:41 -0300

    Correção indentação

--- pilha/pilha_contrato/Empilhavel.java
+++ pilha/pilha_contrato/Empilhavel.java
@@ -1,21 +1,16 @@
 package pilha_contrato;
 
-//Stack (pilha) - Contrato (código genérico)
+//Stack(pilha) - Contrato(código genérico)
 public interface Empilhavel {
-  //Operações principais
+	//Operações principais
+	
+	void empilhar(Object dado);		//push
+	Object desempilhar();			//pop
+	Object retornarTopo();			//top
+	void atualizar(Object dados);	//update
 
-  void empilhar(Object dado);		//push
-
-  Object desempilhar();			//pop
-
-  Object retornarTopo();			//top
-
-  void atualizar(Object dados);	//update
-
-  //Operações auxiliares
-  Boolean estaCheia();			//isFull -> overflow
-
-  Boolean estaVazia();			//isEmpty -> underflow
-
-  String imprimir();				//print
+	//Operações auxiliares
+	Boolean estaCheia();			//isFull -> overflow
+	Boolean estaVazia();			//isEmpty -> underflow
+	String imprimir();				//print
 }
