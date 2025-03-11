1fd4a737f516786a1209fe97ad0df798a5a7ff7d
Mescxll <202411190002@ifba.edu.br>
2025-03-11 13:44:35 -0300

    Adição pilha contrato

---
+++ pilha/pilha_contrato/Empilhavel.java
@@ -0,0 +1,21 @@
+package pilha_contrato;
+
+//Stack (pilha) - Contrato (código genérico)
+public interface Empilhavel {
+  //Operações principais
+
+  void empilhar(Object dado);		//push
+
+  Object desempilhar();			//pop
+
+  Object retornarTopo();			//top
+
+  void atualizar(Object dados);	//update
+
+  //Operações auxiliares
+  Boolean estaCheia();			//isFull -> overflow
+
+  Boolean estaVazia();			//isEmpty -> underflow
+
+  String imprimir();				//print
+}
